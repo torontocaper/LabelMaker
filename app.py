bb1336a3a7d51030db8194896db0c3d8ae87a78b
Adam <torontocaper@gmail.com>
2023-05-11 17:11:18 -0400

    made some progress!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import dotenv
 import requests
@@ -7,77 +8,74 @@ from flask import Flask, jsonify, request
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+# get the bot token for authentication and fire up the slack client
 dotenv.load_dotenv()
-
 bot_token = os.environ.get("BOT_TOKEN")
-
 client = WebClient(token=bot_token)
+
 app = Flask(__name__)
+
+# initiate global variables
+app_id = os.environ.get("APP_ID")
+channel_id = ""
+file_id = ""
+timestamp = ""
+# might not need this // has_file = False
+message_text = f"Thanks for the message."
 
 @app.route('/slack/events', methods=['POST'])
 def slack_event_handler():
+    # get the request data in json format
     request_data = request.get_json()
+    print(request_data)
+    # get the event type
     event_type = request_data["event"]["type"]
-    channel = ""
-    file_id = ""
-    timestamp = ""
-    message_text = ""
-    
-    if request_data["event"]["type"] == "message":
-        channel = request_data["event"]["channel"]
+    global message_text
+    global channel_id
+    global file_id
+    global timestamp
+           
+    # get the timestamp and channel ID from the "message" event
+    if event_type == "message":
+        channel_id = request_data["event"]["channel"]
         timestamp = request_data["event"]["ts"]
-        message_text = f"This is a message. Its channel ID is {channel} and its TS is {timestamp}."
 
-        client.chat_postMessage(
-            channel=channel,
-            text=message_text,
-            thread_ts=timestamp
-        )
-
-    elif request_data["event"]["type"] == "file_public":
+    elif event_type == "file_public" or event_type == "file_shared":
         file_id = request_data["event"]["file_id"]
-        message_text = f"This is a file_public. The file ID is {file_id}."
-        
-    elif request_data["event"]["type"] == "file_shared":
-        file_id = request_data["event"]["file_id"]
-        channel = request_data["event"]["channel_id"]
-        message_text = f"This is a file_shared. The file id is {file_id} and the channel is {channel}."
-
-        client.chat_postMessage(
-            channel=channel,
-            text=message_text
-        )
+        file_vtt = get_file_info(file_id)
+        message_text = f"Your file id is {file_id} and your vtt file is at {file_vtt}."
     
+    # avoid infinite loop
+    if "bot_id" not in request_data["event"]:
+        send_message(channel_id, message_text, timestamp)
+        
     return "OK"
     
-    """ print(request_data)
-    if request_data["event"]["type"] == "file_shared": # request_data["event"]["type"] == "file_public" or 
-        channel_id = request_data["event"]["channel_id"]
-        event_ts = request_data["ts"]
-        file_id = request_data["event"]["file_id"]
-        
-        try:
-            result = client.chat_postMessage(
-            channel=channel_id,
-            thread_ts=event_ts,
-            text=f"Thanks for uploading a file! Your file id is {file_id}"
-        )
-            print(result)
-        
-        except SlackApiError as e:
-        
-            print(f"Error posting message: {e}")
+# get the file info
 
-        # file_info = client.files_info(file=file_id)
-        # data = file_info.data
-        # file_data = data["file"]
-        # vtt_link = file_data["vtt"]
-        # print(vtt_link)
-        # user_id = request_data["event"]["user_id"]
-        return "File uploaded and processed"
+def get_file_info(file_id):
+    response = client.files_info(file=file_id)
+    if "vtt" in response["file"]:
+        vtt_link = response["file"]["vtt"]
     else:
-        return "No file uploaded" """
+        time.sleep(1)
+        get_file_info(file_id)
+    return vtt_link
+# send the finished message 
+
+def send_message(channel, message, timestamp):
+    client.chat_postMessage(
+        text=message,
+        channel=channel,
+        thread_ts=timestamp
+    )
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+### THE BELOW CODE HANDLES VERIFICATION REQUESTS -- DON'T DELETE ###
+
+""" if "challenge" in request_data:
+    return request_data["challenge"] """
