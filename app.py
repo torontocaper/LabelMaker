8abc806ac74e74f5b034ad4e0fc0062e7c6c40dc
Adam <torontocaper@gmail.com>
2023-05-19 10:30:10 -0400

    getting some weird behaviour

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 import time
+import datetime
 
 import requests
 from dotenv import load_dotenv
 from flask import Flask, request
 from slack_sdk import WebClient
 
-# from slack_sdk.errors import SlackApiError
-
+# get the environment variables from .env
 load_dotenv()
 
 # get the bot token for authentication and fire up the slack client
@@ -15,13 +15,12 @@ bot_token = os.environ.get('BOT_TOKEN')
 client = WebClient(token=bot_token)
 headers = {"Authorization": "Bearer " + bot_token}
 
-# initiate global variables
-app_id = os.environ.get('APP_ID')
-channel_id = file_id = timestamp = message_text = ""
-
 app = Flask(__name__)
 @app.route('/slack/events', methods=['POST'])
 def slack_event_handler():
+
+    # get the time now
+    print(f"{datetime.datetime.now()}: Request received")
 
     # get the request data in json format
     request_data = request.get_json()
@@ -30,50 +29,77 @@ def slack_event_handler():
     if "challenge" in request_data:
         return request_data["challenge"]
 
-    # get the event id
-    event_id = request_data["event_id"]
+    else:
+        event_id = request_data["event_id"]
+        
+        event_data = request_data["event"]
+        user_id = event_data["user"]
+        reaction_type = event_data["reaction"]
+        
+        item_data = event_data["item"]
+        channel_id = item_data["channel"]
+        timestamp = item_data["ts"]      
+      
+        print(f"{datetime.datetime.now()}: Event: {event_id}; User: {user_id}; Rxn: {reaction_type}; Chan: {channel_id}; TS: {timestamp}.")
 
-    global message_text
-    global channel_id
-    global file_id
-    global timestamp
+        # handle emoji reactions
+        if reaction_type != "label":
+            print(f"{datetime.datetime.now()}: Can't compute :{reaction_type}: reaction!")
+        else:  
+            conversation = client.conversations_replies(
+                channel=channel_id,
+                ts=timestamp
+            )
+            messages = conversation.get("messages")
+            root_message = messages[0]
 
-    # handle emoji reactions
-    reaction_type = request_data["event"]["reaction"]
-    if reaction_type != "label":
-        print(f"Can't compute :{reaction_type}: reaction!")
-    else:        
-        channel_id = request_data["event"]["item"]["channel"]
-        timestamp = request_data["event"]["item"]["ts"]
-        conversation = client.conversations_replies(
-            channel=channel_id,
-            ts=timestamp
-        )
-        messages = conversation.get("messages")
-        root_message = messages[0]
-
-        if "files" in root_message:   
-            file_id = root_message["files"][0]["id"]
-            file_vtt = get_file_info(file_id)
-            save_location = event_id + '.vtt'
-            vtt_file_for_conversion = download_vtt_file(file_vtt, save_location)
-            txt_file_output = event_id + ".txt"
-            finished_txt_file = convert_vtt_to_labels(vtt_file_for_conversion, txt_file_output)
-            client.files_upload_v2(
-            channel=channel_id,
-            thread_ts=timestamp,
-            initial_comment="Thanks for using LabelMaker! Here's your labels file:",
-            file=finished_txt_file
-        )
-            os.remove(vtt_file_for_conversion)
-            os.remove(finished_txt_file)
+            if "files" in root_message:
+                print(f"{datetime.datetime.now()}: File found.")
+                file_id = root_message["files"][0]["id"]
+                file_vtt = get_file_info(file_id)
+                save_location = event_id + '.vtt'
+                vtt_file_for_conversion = download_vtt_file(file_vtt, save_location)
+                txt_file_output = event_id + ".txt"
+                finished_txt_file = convert_vtt_to_labels(vtt_file_for_conversion, txt_file_output)
+                client.files_upload_v2(
+                channel=channel_id,
+                thread_ts=timestamp,
+                initial_comment="Thanks for using LabelMaker! Here's your labels file:",
+                file=finished_txt_file
+            )
+                os.remove(vtt_file_for_conversion)
+                os.remove(finished_txt_file)
+                print(f"{datetime.datetime.now()}: Temporary files deleted.")
+            else:
+                print(f"{datetime.datetime.now()}: No file found.")
 
     return "OK"
 
+def get_file_info(file_id):
+    print(f"{datetime.datetime.now()}: Attempting to get VTT file from Slack.")
+    response = client.files_info(file=file_id)
+    while "vtt" not in response["file"]:
+        print(f"{datetime.datetime.now()}: Slack has not generated a VTT file yet. Trying again...")
+        response = client.files_info(file=file_id)
+    print(f"{datetime.datetime.now()}: Transcript generated, link found.")
+    vtt_link = response["file"]["vtt"]
+    return vtt_link
+
+def download_vtt_file(url, save_path):
+    print(f"{datetime.datetime.now()}: Downloading VTT file.")
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print(f"{datetime.datetime.now()}: VTT file downloaded successfully and saved at: {save_path}")
+    else:
+        print(f"{datetime.datetime.now()}: Failed to download VTT file. Status code: {response.status_code}")
+    return save_path
+
 def convert_vtt_to_labels(vtt_file, labels_file):
+    print(f"{datetime.datetime.now()}: Converting VTT to labels.")
     with open(vtt_file, 'r') as vtt:
         vtt_lines = vtt.readlines()
-
     labels = []
     for line in vtt_lines:
         line_index = vtt_lines.index(line)
@@ -91,28 +117,8 @@ def convert_vtt_to_labels(vtt_file, labels_file):
     with open(labels_file, 'w') as labels_out:
         labels_out.write(''.join(labels))
 
-    print(f'Successfully converted {vtt_file} to Audacity labels format.')
+    print(f'{datetime.datetime.now()}: Successfully converted {vtt_file} to Audacity labels format.')
     return labels_file
-
-def download_vtt_file(url, save_path):
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print(f"VTT file downloaded successfully and saved at: {save_path}")
-    else:
-        print(f"Failed to download VTT file. Status code: {response.status_code}")
-    return save_path
-
-def get_file_info(file_id):
-    response = client.files_info(file=file_id)
-    if "vtt" in response["file"]:
-        vtt_link = response["file"]["vtt"]
-    else:
-        print("No transcript generated yet. Trying again in five seconds.")
-        time.sleep(5)
-        get_file_info(file_id)
-    return vtt_link
 
 if __name__ == '__main__':
     app.run(debug=True)
